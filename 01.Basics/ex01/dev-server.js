7298eb39035715695d3fdf23faa1fa374681001a
Godbell <12191579@inha.edu>
2025-02-07 11:46:02 +0900

    Learn: 01/ex01

--- 01.Basics/ex01/dev-server.js
+++ 01.Basics/ex01/dev-server.js
@@ -1,12 +1,15 @@
-const http = require('http');
-const path = require('path');
-const express = require('express');
+import http from 'http';
+import path from 'path';
+import express from 'express';
 
 const port = 3000;
-const application = express().use(express.static(path.join(__dirname, 'public')))
+const application = express().use(
+  express.static(path.join(import.meta.dirname, 'public')),
+);
+
 http
-    .createServer(application)
-    .on('listening', ()=>{
-        console.log(`server starts...on ${port}`);
-    })
-    .listen(port);
+  .createServer(application)
+  .on('listening', () => {
+    console.log(`server starts...on ${port}`);
+  })
+  .listen(port);
