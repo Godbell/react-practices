a4ccb9bfaff9fac5513817f8c20aa5cffd631f34
Godbell <12191579@inha.edu>
2025-02-13 15:54:48 +0900

    Feat: impl checkbox

--- 13.Integration/kanbanboard/frontend/src/components/Task.js
+++ 13.Integration/kanbanboard/frontend/src/components/Task.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /**
  * @param {TaskData} props
  * @returns {React.JSX.Element}
  */
 export const Task = ({ no, name, done }) => {
+  const [isDone, setIsDone] = useState(done);
+
   return (
     <li className="_Task">
-      <input type="checkbox" checked={done} />
+      <input
+        type="checkbox"
+        checked={isDone}
+        onChange={() => setIsDone((prev) => !prev)}
+      />
       {name}
       <a href="#" className="Task_Remove"></a>
     </li>
