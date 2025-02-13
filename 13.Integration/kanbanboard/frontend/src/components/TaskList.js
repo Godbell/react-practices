6a9073679b89f16325cc3767495579108971af00
Godbell <12191579@inha.edu>
2025-02-13 15:46:38 +0900

    Fix: correct layout

--- 13.Integration/kanbanboard/frontend/src/components/TaskList.js
+++ 13.Integration/kanbanboard/frontend/src/components/TaskList.js
@@ -10,10 +10,11 @@ import { Task } from './Task';
 export const TaskList = ({ tasks }) => {
   return (
     <div className="Task_List">
-      <ul>tasks</ul>
-      {tasks.map((task) => (
-        <Task key={task.no} no={task.no} name={task.name} done={task.done} />
-      ))}
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task.no} no={task.no} name={task.name} done={task.done} />
+        ))}
+      </ul>
       <input className="Input_Add_Task" type="text" placeholder="태스크 추가" />
     </div>
   );
